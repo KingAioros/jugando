30b68de0571f271b4f0faf3750d734747134b0ff
Maty A <matt_ezequiel@hotmail.com>
2023-03-18 20:21:40 -0300

    cambios parte 4

--- js/contacto.js
+++ js/contacto.js
@@ -1,3 +1,21 @@
+//Variables
+let persona = 0;
+let email = false;
+let formEnviar = document.getElementById("fEnviarCont");
+//Se crean objetos
+let crearPersona = () => { persona = new Persona ({ nombre: userName, apellido: userLastName, usuario: user, email: userMail, phone: userPhone });}
+
+//Object
+class Persona{
+  constructor(literal){
+    this.nombre = literal.nombre;
+    this.apellido = literal.apellido;
+    this.usuario = literal.usuario;
+    this.email = literal.email;
+    this.phone = literal.phone;
+  }
+}
+
 function contacto () { 
 Swal.fire({
     confirmButtonColor : "#ffb866",
@@ -39,3 +57,81 @@ Swal.fire({
   </div>`,
 });
 }
+
+
+    //Alerta mail
+const alertMail = (leyenda,correo,icon) => {
+    Swal.fire({
+    title: `'Correo ${leyenda}'`,
+    text: `"La dirección de email ${correo} es ${leyenda}!."`,
+    icon: `${icon}`,
+    showConfirmButton: true,
+  })
+    }
+
+const datosError = (mensaje) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        showConfirmButton: true,
+        text: `${mensaje}`,
+      })
+}
+
+const registroExitoso = () => {
+  Swal.fire({
+    toast: true,
+    position: 'top-end',
+    icon: 'success',
+    title: 'Registro completado con exito',
+    showConfirmButton: true,
+    timer: 1500,
+    timerProgressBar: true
+  })
+  window.location="../page/loginWelcome.html";
+}
+
+    //Validar correo
+const validarEmail = (valor) => {
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+       alertMail("válido", valor, 'success');
+     email = true;
+    } else {
+       alertMail("invalido", valor, 'error');
+     email = false;
+    }
+   }
+
+   //Registrar datos
+function registrar(){
+     userName = document.getElementById("name").value;
+     userLastName = document.getElementById("lastName").value;
+     user = document.getElementById("usuario").value;
+     userMail = document.getElementById("mail").value;
+     userPhone = document.getElementById("phone").value;
+
+    if (userName && userLastName && user &&userMail && userPhone != " "){
+        validarEmail(userMail)
+        if (email == true){
+        crearPersona();
+        const enJSON = JSON.stringify(persona);
+        localStorage.setItem(`Usuario:${user}`, enJSON)
+        localStorage.setItem(`Correo:${userMail}`, enJSON)
+        registroExitoso();
+        }
+        else {
+        console.log("correo invalido")
+        }
+    }
+    else if(userPassword != userPasswordVerificar){
+        datosError("Verifique el password");
+    }
+    else {
+        datosError("Contiene campos sin completar");
+    }
+}
+
+//Ejecutar codigo
+ formEnviar.onclick = () => {
+    registrar();
+ }
